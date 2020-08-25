379cb533dbd7e50322e93f1a86604c6594c6ed83
dlambert-eworking <lambert.eworking@free.fr>
2020-08-25 13:26:31 +0000

    add bucket

--- gcp/DataFlow/deploy.sh
+++ gcp/DataFlow/deploy.sh
@@ -3,4 +3,10 @@
 echo "Enable API"
 gcloud services enable dataflow.googleapis.com pubsub.googleapis.com 
 gcloud services list
+
+echo "Cloud storage : Création des bucket"
+gsutil mb -b on -c Standard -l EUROPE-WEST1 gs://cs-for-dataflow-dla
+
 echo "Fin"
+
+echo "Enable API"
