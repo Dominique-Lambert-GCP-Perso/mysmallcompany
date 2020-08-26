a7fcd84dd3e4c8affb8c31d59ee3e8bfa395ef0d
dlambert-eworking <lambert.eworking@free.fr>
2020-08-26 09:07:05 +0000

    add deploy files

--- gcp/DataFlow/SQLPubSub#1/clean.sh
+++ gcp/DataFlow/SQLPubSub#1/clean.sh
@@ -3,4 +3,10 @@
 echo "delete buckets"
 gsutil -m rm -r gs://cs-for-dataflow-dla
 
+echo "delete Pub/sub topic"
+gcloud pubsub topics delete transactions
+
+echo "delete Dataset"
+bq rm -f data-flow-test-dla:dataflow_sql_dataset
+
 echo "Fin"
--- gcp/DataFlow/SQLPubSub#1/deploy.sh
+++ gcp/DataFlow/SQLPubSub#1/deploy.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 echo "Enable API"
-gcloud services enable dataflow.googleapis.com pubsub.googleapis.com 
+gcloud services enable dataflow.googleapis.com pubsub.googleapis.com datacatalog.googleapis.com
 gcloud services list
 
 echo "Cloud storage : Création des bucket"
 gsutil mb -b on -c Standard -l EUROPE-WEST1 gs://cs-for-dataflow-dla
+
+echo "Create Pub/Sub topic"
+gcloud pubsub topics create transactions
+
+echo "BigQuery : create dataset" 
+bq --location EU mk --dataset --description "dataset pour dataflow SQLPubSub#1" data-flow-test-dla:dataflow_sql_dataset
 
 echo "Fin"
