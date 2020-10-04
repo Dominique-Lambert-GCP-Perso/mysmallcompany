a4bb7bd25a3e151d351b6de2f47912e2412c061f
dlambert-eworking <lambert.eworking@free.fr>
2020-10-04 16:16:54 +0000

    modification des configurations node-master pour pouvoir lancer spark-shell

--- GCP/DataProc/singlenode#3/deploy.sh
+++ GCP/DataProc/singlenode#3/deploy.sh
@@ -13,6 +13,7 @@ gcloud compute --project=data-proc-test-dla firewall-rules create vpc-dataproc-a
 gcloud compute --project=data-proc-test-dla firewall-rules create vpc-dataproc-allow-ssh --direction=INGRESS --priority=1000 --network=vpc-dataproc --action=ALLOW --rules=tcp:22 --source-ranges=0.0.0.0/0
 
 echo "Création du cluster"
-gcloud dataproc clusters create cluster-dataproc-dla --optional-components=ANACONDA,DRUID,ZOOKEEPER,HBASE,HIVE_WEBHCAT,JUPYTER --enable-component-gateway --bucket cs-for-dataproc-dla --temp-bucket cs-for-dataproc-dla-temp --region europe-west1 --subnet subnet-dataproc --no-address --zone europe-west1-b --single-node --master-machine-type n1-standard-1 --master-boot-disk-size 500 --image-version 1.3-debian10 --project data-proc-test-dla
+#gcloud dataproc clusters create cluster-dataproc-dla --optional-components=ANACONDA,DRUID,ZOOKEEPER,HBASE,HIVE_WEBHCAT,JUPYTER --enable-component-gateway --bucket cs-for-dataproc-dla --temp-bucket cs-for-dataproc-dla-temp --region europe-west1 --subnet subnet-dataproc --no-address --zone europe-west1-b --single-node --master-machine-type n1-standard-1 --master-boot-disk-size 500 --image-version 1.3-debian10 --project data-proc-test-dla
+gcloud dataproc clusters create cluster-dataproc-dla --enable-component-gateway --bucket cs-for-dataproc-dla --temp-bucket cs-for-dataproc-dla-temp --region europe-west1 --subnet subnet-dataproc --no-address --zone europe-west1-b --single-node --master-machine-type n1-standard-2 --master-boot-disk-size 500 --image-version 1.3-debian10 --project data-proc-test-dla
 
 echo "Fin"
