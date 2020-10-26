9fc0f7abc40a4719623abc54d56b1d99b9922d8c
dlambert-eworking <lambert.eworking@free.fr>
2020-10-26 12:37:52 +0000

    première version fonctionnelle de main.tf

--- GCP/Terraform/main.tf
+++ GCP/Terraform/main.tf
@@ -17,20 +17,20 @@ resource "google_compute_instance" "vm_instance" {
 
   network_interface {
     # A default network is created for all GCP projects
-    network = google_compute_network.terraform-network.self_link
+    network = google_compute_network.vpc_network.self_link
     access_config {
     }
   }
 }
 
-resource "google_compute_network" "terraform-network" {
+resource "google_compute_network" "vpc_network" {
   name                    = "terraform-network"
   auto_create_subnetworks = "true"
 }
 
-resource "google_compute_firewall" "terraform-network" {
+resource "google_compute_firewall" "FireWall_rule" {
   name    = "terraform-network-allow-ssh"
-  network = google_compute_network.terraform-network
+  network = google_compute_network.vpc_network.name
 
   allow {
     protocol = "tcp"
